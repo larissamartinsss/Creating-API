a496b0d22c19ad03dcd85b6385b5c3933086ac9d
Larissa Martins <larissamartins.job@gmail.com>
2022-01-10 13:52:27 -0400

    module restAPi OK

--- API-GAMES/index.js
+++ API-GAMES/index.js
@@ -36,7 +36,7 @@ app.get("/games", (req, res) => {
     res.json(DB.games)
 });
 
-app.get("/games/:id", (req, res) => {
+app.get("/game/:id", (req, res) => {
 
     if(isNaN(req.params.id)){
         res.sendStatus(400);
@@ -49,6 +49,69 @@ app.get("/games/:id", (req, res) => {
         if(game != undefined){
             res.statusCode = 200;
             res.json(game);
+        }else{
+            res.sendStatus(404);
+        }
+    }
+})
+
+app.post("/game", (req, res) => {
+    var {title, price, year} = req.body;
+
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year
+    });
+    res.sendStatus(200)
+});
+
+app.delete("/game/:id", (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+
+    }else{
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id ==id);
+
+        if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index, 1);
+            res.sendStatus(200);
+        }
+      
+    }
+});
+
+app.put("/game/:id", (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+           
+            var{title, price, year} = req.body;
+
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            
+            if(year != undefined){
+                game.year = year;
+            }
+
+            res.sendStatus(200);
+
+
         }else{
             res.sendStatus(404);
         }

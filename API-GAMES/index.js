187fbc468f73165169bc3cc328d0b4abe590d707
Larissa Martins <larissamartins.job@gmail.com>
2022-01-24 23:42:44 -0400

    jwt

--- API-GAMES/index.js
+++ API-GAMES/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 
